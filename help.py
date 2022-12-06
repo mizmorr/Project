9a4823688b9b533818577af1a58db6532f77f94b
mizmor <mizmorrr@gmail.com>
2022-12-06 03:45:19 +0300

    myrandom done

--- help.py
+++ help.py
@@ -1,23 +1,44 @@
-import ctypes, time
-so = ctypes.cdll.LoadLibrary('./lib.so')
+#!/usr/bin/python
+# import ctypes, time
+# so = ctypes.cdll.LoadLibrary('./lib.so')
 
-        self.pushButton_6.released.connect(self.btn_go)
-    def btn_go(self):
-            fs = so.firstSample 
-            fs = so.firstSample
-            num = self.spinBox_6.value()
-            fs.restype = ctypes.c_void_p
-            fs.argtypes=[ctypes.c_int]
-            fsout = fs(num)
-            self.progressBar_6.setValue(0)
-            time.sleep(0.025)
-            self.progressBar_6.setValue(100)
-            fbytes = ctypes.string_at(fsout)
-            fstring = fbytes.decode('utf-8')
-            farr  = fstring.split('\n')
-            list.reverse(farr)
-            time2 = list.pop(farr)
-            list.reverse(farr)
-            fstring = '\n'.join(farr) 
-            self.label_24.setText(time2)
-            self.plainTextEdit_2.setPlainText(fstring)
+#     self.pushButton_6.released.connect(self.btnFirst_go)
+#         self.pushButton_7.released.connect(self.btnSecond_go)
+
+#     def btnFirst_go(self):
+#             fs = so.firstSample 
+#             num = self.spinBox_6.value()
+#             fs.restype = ctypes.c_void_p
+#             fs.argtypes=[ctypes.c_int]
+#             fsout = fs(num)
+#             self.progressBar_6.setValue(0)
+#             time.sleep(0.025)
+#             self.progressBar_6.setValue(100)
+#             fbytes = ctypes.string_at(fsout)
+#             fstring = fbytes.decode('utf-8')
+#             time2,fstring = strconv(fstring)
+#             if fstring=="no such k-core found\n":
+#                  self.label_18.setText("")
+#             else:
+#                 self.label_18.setText(time2)
+#             self.plainTextEdit_2.setPlainText(fstring)
+    
+#     def btnSecond_go(self):
+#             fs = so.SecondSample 
+#             num = self.spinBox_7.value()
+#             fs.restype = ctypes.c_void_p
+#             fs.argtypes=[ctypes.c_int]
+#             fsout = fs(num)
+#             self.progressBar_7.setValue(0)
+#             time.sleep(0.025)
+#             self.progressBar_7.setValue(100)
+#             fbytes = ctypes.string_at(fsout)
+#             fstring = fbytes.decode('utf-8')
+#             time2,fstring = strconv(fstring)
+#             if fstring=="no such k-core found\n":
+#                  self.label_24.setText("")
+#             else:
+#                 self.label_24.setText(time2)
+#             self.plainTextEdit_3.setPlainText(fstring)
+import subprocess
+subprocess.Popen(r'explorer /select,"/home"')
--- main.go
+++ main.go
@@ -21,26 +21,43 @@ func firstSample(num C.int) *C.char {
 
 //export BigDensity
 func BigDensity(v, num C.int) *C.char {
+	start := time.Now()
 	var g = graphCore.RandomBG_Graph(int(v), int(num))
-	return C.CString(g)
+	duration := time.Since(start)
+	dur := fmt.Sprintln(duration.Seconds())
+	res := dur + g
+	return C.CString(res)
 }
 
 //export Sparse
 func Sparse(v, num C.int) *C.char {
+	start := time.Now()
 	var g = graphCore.RandomS_Graph(int(v), int(num))
-	return C.CString(g)
+	duration := time.Since(start)
+	dur := fmt.Sprintln(duration.Seconds())
+	res := dur + g
+	return C.CString(res)
 }
 
 //export Erdos_Renyi
 func Erdos_Renyi(prob C.float, v, num C.int) *C.char {
+	start := time.Now()
 	var g = graphCore.Erdos_Renyi(int(v), float32(prob), int(num))
-	return C.CString(g)
+	duration := time.Since(start)
+	dur := fmt.Sprintln(duration.Seconds())
+	res := dur + g
+	return C.CString(res)
 }
 
 //export Last_Sample
 func Last_Sample(num C.int) *C.char {
+
+	start := time.Now()
 	var g = graphCore.Lastfm_Sample(int(num))
-	return C.CString(g)
+	duration := time.Since(start)
+	dur := fmt.Sprintln(duration.Seconds())
+	res := dur + g
+	return C.CString(res)
 }
 
 //export SecondSample
